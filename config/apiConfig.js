3327bfe74efa9bcb9c6809e9326818ef44b0db8e
aryan devops <tomar.aryan16j@gmail.com>
2026-03-27 17:31:56 +0530

    second

--- config/apiConfig.js
+++ config/apiConfig.js
@@ -1,5 +1,5 @@
 export const SERVER_URL = "https://red.bankit.in";
-  // export const SERVER_URL = "http://localhost:8001";
+// export const SERVER_URL = "http://localhost:8001";
 // New API (UAT)
 export const FINDI_INSURE_BASE = "https://uat.bankit.in/BANKITMRA/resources/AESAPI";
 
@@ -7,7 +7,7 @@ export const API_ENDPOINTS = {
   FINDI_INSURE_PLANS: `${SERVER_URL}/BANKITMRA/resources/AESAPI/findiInsurePlan`,
   FINDI_INSURE_OTP: `${SERVER_URL}/BANKITMRA/resources/AESAPI/findiInsureOtp`,
   FINDI_INSURE_REGISTER: `${SERVER_URL}/BANKITMRA/resources/AESAPI/findiInsureRegistration`,
-PG_CALLBACK: `${SERVER_URL}/BANKITMRA/FindiInsurePGResponse.jsp`,
+  PG_CALLBACK: `${SERVER_URL}/BANKITMRA/FindiInsurePGResponse.jsp`,
 };
 
 // export const API_ENDPOINTS = {
